8a28843f0a65d6ce086b84915a2d8162c6012dd1
laysgabrielle <130411960+laysgabrielle@users.noreply.github.com>
2024-05-22 12:58:20 -0300

    tela selecao

---
+++ src/components/selecao-aluno.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface CardSelecaoProps {
+  title: string;
+}
+
+const CardSelecao: React.FC<CardSelecaoProps> = ({ title }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "orange",
+    width: 240,
+    height: 90,
+    borderRadius: 10,
+    justifyContent: 'center', // Centraliza o texto verticalmente
+    alignItems: 'center', // Centraliza o texto horizontalmente
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default CardSelecao;
