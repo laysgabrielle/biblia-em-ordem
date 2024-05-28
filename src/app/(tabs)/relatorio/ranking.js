0be3727826381b5ca0e2c526fc6436f227bfcde2
danilo <daniloborges@unipam.edu.br>
2024-05-28 16:11:58 -0300

    feat: grafico

--- src/app/(tabs)/relatorio/ranking.js
+++ src/app/(tabs)/relatorio/ranking.js
@@ -1,11 +1,12 @@
 import { Text, View } from "react-native"
+import Grafico from  "../../../components/grafico"
 
 export default function Ranking()
 {
     return(
         <View className="flex-1 items-center justify-center">
         <Text>Tela ranking</Text>
-
+        <Grafico />
     </View>
     )
 }
---
+++ src/components/grafico.tsx
@@ -0,0 +1,57 @@
+import { BarChart } from "react-native-gifted-charts";
+import { View } from "react-native";
+import db from "../../firebase/firebaseConfig";
+import { collection, where, Timestamp, query, getDocs, getDoc } from "firebase/firestore";
+import { Domingos } from "../helpers/domingos";
+import { useEffect, useState } from "react";
+
+
+
+
+const Grafico = () => {
+    const [dadosObtained, setDadosObtained] = useState<any[]>([]);
+    useEffect(() => {
+        console.log("Entrou no useEffect")
+        getDadosPorDia();
+        tratarDados();
+    }, []);
+    
+    async function getDadosPorDia() {
+    try{    
+        console.log("Entrou no getDadosPorDia")
+        const q = query(
+            collection(db, "dados_obtained"),
+            where("dia_letivo", ">=", Timestamp.fromDate(new Date("2024-05-25"))),
+            where("dia_letivo", "<=", Timestamp.fromDate(new Date("2024-05-27")))
+        );
+        const querySnapshot = await getDocs(q);
+        const data = await Promise.all(querySnapshot.docs.map(async doc => {
+            const fullDoc = await getDoc(doc.ref);
+            return { id: doc.id, ...fullDoc.data() };
+        }))
+        setDadosObtained(data);
+    }
+        catch(e){
+            console.log(e);
+        }
+    }
+    
+    function tratarDados(){
+        console.log("Entrou no tratarDados")
+        //Sort por biblias
+        dadosObtained.sort((a, b) => b.biblias - a.biblias);
+        dadosObtained.map((item) => {
+            console.log(item.biblias + " " + item.turma_referente);
+        });
+        //Sort por visitantes
+        dadosObtained.sort((a, b) => b.visitantes - a.visitantes);
+        dadosObtained.map((item) => {
+            console.log(item.visitantes + " " + item.turma_referente);
+        });
+    }
+    const barraData = dadosObtained.map((item) => ({value: item.biblias}))
+    return <BarChart data={barraData}/>;
+
+};
+
+export default Grafico;
